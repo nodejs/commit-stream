66e96a942013df725d5ba7ab93a72b2a233ecf96
Rod Vagg <rod@vagg.org>
2015-07-22 13:28:00 +1000

    prepare for release

---
+++ README.md
@@ -0,0 +1,34 @@
+# commit-stream
+
+**Turn a `git log` into a stream of commit objects**
+
+[![npm](https://nodei.co/npm/commit-stream.png?downloads=true&downloadRank=true)](https://nodei.co/npm/commit-stream/)
+[![npm](https://nodei.co/npm-dl/commit-stream.png?months=6&height=3)](https://nodei.co/npm/commit-stream/)
+
+## API
+
+**`commitStream([defaultGithubUser, defaultGithubProject])`**
+
+Returns an object stream that you can pipe the output of `git log` to. The output of the stream is a series of objects, each representing a commit from the log.
+
+Intended to be used on a standard log format (not a simplified one using one of the simplifying `--format` options) but it will also process change stats if you `git log --stat`.
+
+Commit object properties:
+
+* `sha`: the full commit sha
+* `author`: an object representing the author of the commit
+  - `name`: the name of the committer
+  - `email`: the email of the committer
+* `summary`: the one-line summary of the commit
+* `description`: the free-form text content of the summary, minus specific metadata lines
+* `reviewers`: an array containing objects with `name` and `email` properties if the commit metadata contains reviewers in a `Reviewed-By: Foo Bar <baz@boom>` format.
+* `changes`: if `--stat` data is included in the git log, an object containing change stats:
+  - `files`: the number of files changed
+  - `insertions`: the number of new lines inserted
+  - `deletions`: the number of old lines removed
+* `prUrl`: a URL pointing to a pull-request where this change was made if the commit metadata contains a `PR-URL: https://github.com/user/project/pull/XX` line. Note that whatever proceeds the `PR-URL: ` string will be collected in this property; one exception being that if a shortened `#XX` version is found and you have supplied `defaultGitHubUser` and `defaultGitHubProject` arguments to the constructor then a full GitHub pull-request will be reconstituted in its place.
+* `ghUser`, `ghProject, `ghIssue`: if a proper GitHub pull request is found for the `prUrl` property (including shortened `#XX` ones), these properties will be added to point to the original user, project and issue (pull-request) for this change, as extracted from the URL.
+
+## License
+
+**commit-stream** is Copyright (c) 2015 Rod Vagg [@rvagg](https://twitter.com/rvagg) and licenced under the MIT licence. All rights not explicitly granted in the MIT license are reserved. See the included LICENSE.md file for more details.
--- test.js
+++ test.js
@@ -14,36 +14,80 @@ function gitToList (gitCmd, callback) {
 
 test('current plain commit log', function (t) {
   gitToList('git log', function (err, list) {
-    t.error(err)
+    t.error(err, 'no error')
 
     t.ok(list && list.length > 1, 'got a list')
-    t.deepEqual(list[0], {
+
+    t.deepEqual(list[list.length - 4], {
+        author: { email: 'rod@vagg.org', name: 'Rod Vagg' }
+      , sha: 'f92b93c3c7175b07f847dd45058b121cea6b3a20'
+      , summary: 'deleted package.json line'
+    }, 'got correct fourth commit')
+
+    t.deepEqual(list[list.length - 3], {
+        author: { email: 'rod@vagg.org', name: 'Rod Vagg' }
+      , description: [ 'comment', 'Reviewed-By: Nobody' ]
+      , sha: 'db34ce2af09a6a9fb70241d43965a2bc48b90b4c'
+      , summary: 'squished package.json'
+    }, 'got correct third commit')
+
+    t.deepEqual(list[list.length - 2], {
+        author      : { email: 'rod@vagg.org', name: 'Rod Vagg' }
+      , description : [
+            'Some extra summary information here'
+          , 'newline'
+          , 'Fixes: https://github.com/rvagg/commit-stream/issues/0'
+        ]
+      , ghIssue     : 0
+      , ghProject   : 'commit-stream'
+      , ghUser      : 'rvagg'
+      , prUrl       : 'https://github.com/rvagg/commit-stream/pulls/0'
+      , reviewers   : [ { email: 'roger@vagg.org', name: 'Roger Vagg' } ]
+      , sha         : '6275758f597ae579202fbe4eccca1407f2c67ac1'
+      , summary     : 'added test.js'
+    }, 'got correct second commit')
+
+    t.deepEqual(list[list.length - 1], {
         sha    : 'd94841274e2979e7758413a0f48fa37560d0dde6'
       , author : { name: 'Rod Vagg', email: 'rod@vagg.org' }
       , summary: 'make it so'
     }, 'got correct first commit')
-    t.deepEqual(list[1], {
-        sha    : 'd94841274e2979e7758413a0f48fa37560d0dde6'
-      , author : { name: 'Rod Vagg', email: 'rod@vagg.org' }
-      , summary: 'added test.js'
-    }, 'got correct second commit')
+
     t.end()
   })
 })
 
 
-/*
-test('current plain commit log', function (t) {
-  gitToList('git log', function (err, list) {
-    t.error(err)
+test('current commit log with changes', function (t) {
+  gitToList('git log --stat', function (err, list) {
+    t.error(err, 'no errors')
 
     t.ok(list && list.length > 0, 'got a list')
-    t.deepEqual(list[0], {
-        sha    : 'd94841274e2979e7758413a0f48fa37560d0dde6'
-      , author : { name: 'Rod Vagg', email: 'rod@vagg.org' }
-      , summary: 'make it so'
-    }, 'got correct first commit')
+
+    t.deepEqual(list[list.length - 4].changes, {
+        files      : 1
+      , insertions : 0
+      , deletions  : 1
+    }, 'got correct second commit changes')
+
+    t.deepEqual(list[list.length - 3].changes, {
+        files      : 1
+      , insertions : 1
+      , deletions  : 28
+    }, 'got correct second commit changes')
+
+    t.deepEqual(list[list.length - 2].changes, {
+        files      : 1
+      , insertions : 49
+      , deletions  : 0
+    }, 'got correct second commit changes')
+
+    t.deepEqual(list[list.length - 1].changes, {
+        files      : 1
+      , insertions : 28
+      , deletions  : 0
+    }, 'got correct first commit changes')
+
     t.end()
-  }))
+  })
 })
-*/
