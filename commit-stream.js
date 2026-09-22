3018c300748442a1a176d379aa904c9fc9a13670
RafaelGSS <rafael.nunu@hotmail.com>
2026-09-22 13:36:45 -0300

    fix: accept CVE IDs with 4 or more sequence digits

--- commit-stream.js
+++ commit-stream.js
@@ -46,7 +46,7 @@ export default function commitStream (ghUser, ghProject) {
         commit.reviewers = []
       }
       commit.reviewers.push({ name: m[1], email: m[2] })
-    } else if ((m = line.match(/\bCVE-ID:\s+(CVE-\d{4}-\d{5})\b/)) !== null) {
+    } else if ((m = line.match(/\bCVE-ID:\s+(CVE-\d{4}-\d{4,})\b/)) !== null) {
       commit.cveId = m[1];
     } else if ((m = line.match(/^\s+PR(?:[- ]?URL)?:?\s*(.+)\s*$/) || line.match(/\(#(\d+)\)$/)) !== null) {
       commit.prUrl = m[1]
